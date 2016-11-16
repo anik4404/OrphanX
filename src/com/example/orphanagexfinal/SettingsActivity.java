67cbf20d9c776eeff3e6fb6f8b9e02249eaa6632
Surya Aniketh Tamirisa <Surya Aniketh Tamirisa>
2016-11-16 06:22:08 +0530

    first commit

---
+++ src/com/example/orphanagexfinal/SettingsActivity.java
@@ -0,0 +1,25 @@
+package com.example.orphanagexfinal;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceActivity;
+import android.util.StringBuilderPrinter;
+import com.surya.orphanagexfinal.R;
+
+public class SettingsActivity extends PreferenceActivity {
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_general);
+
+        /*ListPreference listPreference = (ListPreference) findPreference("city");
+        CharSequence currText = listPreference.getEntry();
+        String currValue = listPreference.getValue();*/
+
+
+    }
+}
